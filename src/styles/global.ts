eda74761d6457d14cb8507bde184970d38764a58
rvmelo <robertomelo7@hotmail.com>
2024-02-15 23:58:55 -0300

    first commmit

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+:focus {
+    outline: none;
+  }
+
+  body {
+    background: ${({ theme }) => theme['base-background']};
+    color: ${({ theme }) => theme['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+`
